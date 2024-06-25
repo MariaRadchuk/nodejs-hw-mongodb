a87835aab4dfd2e1efc30a36d995599ceb3e3fc6
MariaRadchuk <mradchuk_15@ukr.net>
2024-06-25 12:22:38 +0300

    1

---
+++ src/utils/ctrlWrapper.js
@@ -0,0 +1,10 @@
+//Wrapper for Controllers
+export const ctrlWrapper = (controller) => {
+  return async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
