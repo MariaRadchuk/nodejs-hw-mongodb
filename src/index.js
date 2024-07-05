003826880bb9f8ba78b82d82c589d69f84583e3e
MariaRadchuk <mradchuk_15@ukr.net>
2024-07-05 22:11:32 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-import { createDirIfNotExist } from './utils/createDirlfNotExist.js';
+import { createDirIfNotExist } from './utils/createDirIfNotExist.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
