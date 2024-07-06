3b255259d6caab932ca3b6d0d61d0483e904abbb
MariaRadchuk <mradchuk_15@ukr.net>
2024-07-06 16:44:24 +0300

    7

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -17,32 +17,22 @@ import { upload } from '../middlewares/multer.js';
 
 const router = Router();
 
-//authenticate
 router.use(authenticate);
 
-//Route to get all contacts we (removed "/contacts")
 router.get('/', ctrlWrapper(getContactsController));
-
-// Route to get a contact by ID (removed "/contacts")
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
-
-// Route for create a contact (removed "/contacts")
 router.post(
   '/',
   upload.single('photo'),
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
-
-// Route for partialy update (PATCH) a contact (removed "/contacts")
 router.patch(
   '/:contactId',
   upload.single('photo'),
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
-
-// Route for delete a contact (removed "/contacts")
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,15 +3,15 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  email: Joi.string().min(3).max(20).optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().min(3).max(20),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  name: Joi.string().min(3).max(20).optional(),
+  phoneNumber: Joi.string().min(3).max(20).optional(),
+  email: Joi.string().min(3).max(20).optional(),
+  isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('work', 'home', 'personal').optional(),
 });
